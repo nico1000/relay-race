7696b90d599f6bef94b476feae09e23c417388e9
Nico Bernold <nb@feinheit.ch>
2017-05-08 22:56:06 +0200

    adding teams and runners

--- relay/admin.py
+++ relay/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django import forms
 
-from .models import Event, Stage, EventStage
+from . import models
 
 
 class StageAdmin(admin.ModelAdmin):
@@ -8,7 +9,7 @@ class StageAdmin(admin.ModelAdmin):
 
 
 class EventStageInline(admin.TabularInline):
-    model = EventStage
+    model = models.EventStage
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -17,8 +18,32 @@ class EventAdmin(admin.ModelAdmin):
     ]
 
 
-    
+class RunnerAdminForm(forms.ModelForm):
 
-admin.site.register(Stage, StageAdmin)
-admin.site.register(Event, EventAdmin)
-admin.site.register(EventStage)
+    class Meta:
+        model = models.Runner
+        exclude = []
+
+    def __init__(self, *args, **kwargs):
+        super(RunnerAdminForm, self).__init__(*args, **kwargs)
+        self.fields['event_stage'].queryset = models.EventStage.objects.filter(event=self.instance.team.event)
+
+
+class RunnerAdmin(admin.ModelAdmin):
+    form = RunnerAdminForm
+
+
+class RunnerInline(admin.TabularInline):
+    model = models.Runner
+
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [
+        RunnerInline,
+    ]
+
+admin.site.register(models.Stage, StageAdmin)
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.EventStage)
+admin.site.register(models.Runner, RunnerAdmin)
+admin.site.register(models.Team, TeamAdmin)
--- relay/models.py
+++ relay/models.py
@@ -3,32 +3,24 @@ from django.utils.translation import ugettext as _
 
 
 class Stage(models.Model):
-    number = models.PositiveSmallIntegerField(
-        _('Number'),
-    )
-    title = models.CharField(
-        _('Title'),
+    name = models.CharField(
         max_length=200,
     )
     distance = models.DecimalField(
-        _('Distance [km]'),
+        help_text=_('distance in kilometers'),
         max_digits=5,
         decimal_places=3,
     )
 
     def __str__(self):
-        return '%s. %s' % (self.number, self.title)
-
+        return '%s (%s km)' % (self.name, self.distance)
 
 
 class Event(models.Model):
     name = models.CharField(
-        _('Name'),
         max_length=100,
     )
-    date = models.DateField(
-        _('Date'),
-    )
+    date = models.DateField()
     stages = models.ManyToManyField(
         Stage,
         through='EventStage',
@@ -48,7 +40,40 @@ class EventStage(models.Model):
         on_delete=models.CASCADE,
     )
     ordering = models.PositiveSmallIntegerField()
-    
+
     def __str__(self):
         return '%s, %s. %s' % (self.event, self.ordering, self.stage)
-        
+
+    class Meta:
+        ordering = ['ordering']
+
+
+class Team(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    event = models.ForeignKey(
+        Event
+    )
+
+    def __str__(self):
+        return '%s - %s' % (self.name, self.event)
+
+
+class Runner(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    time_estimated = models.DurationField(
+        help_text=_('hh:mm:ss'),
+    )
+    team = models.ForeignKey(
+        Team
+    )
+    event_stage = models.ForeignKey(
+        EventStage,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
